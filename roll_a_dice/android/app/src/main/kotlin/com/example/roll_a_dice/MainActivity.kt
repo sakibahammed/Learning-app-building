cc303090af4cc81ae26eaf234c9e8be6eeb0ab8c
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2025-07-24 18:32:29 -0500

    first commit

---
+++ roll_a_dice/android/app/src/main/kotlin/com/example/roll_a_dice/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.roll_a_dice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ roll_a_dice/lib/main.dart
@@ -0,0 +1,8 @@
+// we need to import the flutter package
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp();
+}
+
+// executing a function , Provided by flutter
