1e97308f9a925051892a9ce069f3597a91ad3e56
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2025-07-24 19:06:15 -0500

    Did some basic staffs

--- roll_a_dice/lib/main.dart
+++ roll_a_dice/lib/main.dart
@@ -2,10 +2,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp() {}
-  ;
+  runApp(const MaterialApp(home: Text('hello mom')));
 
-  print('hello mom');
+  // run app runs the code , and you need to push the widget name ,and tell what needs to be drawn
+  //material app is the core widgets
+
+  //flutter UI are createed by combining & nesting widgets
 }
 
 // executing a function , Provided by flutter
