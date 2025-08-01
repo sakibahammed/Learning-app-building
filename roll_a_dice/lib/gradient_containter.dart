ddcb7ff1d686d9e3ea5aa06e9a58986a8e57fad7
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2025-08-01 14:04:03 -0500

    done

--- roll_a_dice/lib/gradient_containter.dart
+++ roll_a_dice/lib/gradient_containter.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:roll_a_dice/dice_roller.dart';
 
-var startAlignment = Alignment.topLeft;
-final endAlignment = Alignment.bottomRight;
+// I use this line of code as
+
+// var startAlignment = Alignment.topLeft;
+// final endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   GradientContainer({super.key});
 
   @override
   Widget build(context) {
-    startAlignment = Alignment.center;
+    // startAlignment = Alignment.center;
     return Container(
       decoration: BoxDecoration(color: Colors.deepPurple),
 
